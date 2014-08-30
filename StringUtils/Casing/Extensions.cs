0e6b88dc72cfe649dd3a7e00aac3207433445e52
Andrew Seward <awswrd@gmail.com>
2014-08-30 15:12:54 -0700

    Add Casing/Extensions Only has one for now, FirstToUpper

---
+++ StringUtils/Casing/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringUtils.Casing
+{
+    public static class Extensions
+    {
+        public static string FirstToUpper(this string str)
+        {
+            if (str.Length > 1)
+            {
+                return char.ToUpper(str[0]) + str.Substring(1);
+            }
+
+            return str.ToUpper();
+        }
+    }
+}
