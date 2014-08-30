9336d29ac368dfdfe09d78d450ee277dc32701f9
Andrew Seward <awswrd@gmail.com>
2014-08-30 15:58:03 -0700

    Add unit tests project to solution

--- StringUtils/Casing/Extensions.cs
+++ StringUtils/Casing/Extensions.cs
@@ -11,7 +11,19 @@ namespace StringUtils.Casing
         {
             if (str.Length > 1)
             {
+                if (char.IsUpper(str[0])) { return str; }
+
                 return char.ToUpper(str[0]) + str.Substring(1);
+            }
+
+            return str.ToUpper();
+        }
+
+        public static string FirstOnlyToUpper(this string str)
+        {
+            if (str.Length > 1)
+            {
+                return char.ToUpper(str[0]) + new string(str.Substring(1).Select(ch => char.ToLower(ch)).ToArray());
             }
 
             return str.ToUpper();
