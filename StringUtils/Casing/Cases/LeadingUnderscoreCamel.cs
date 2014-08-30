ad1b25b008dc5041ef699b06ed2049db6be3b24e
Andrew Seward <awswrd@gmail.com>
2014-08-30 15:26:34 -0700

    Add LeadingUnderscoreCamel

---
+++ StringUtils/Casing/Cases/LeadingUnderscoreCamel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringUtils.Casing.Cases
+{
+    public class LeadingUnderscoreCamel : Camel
+    {
+        public override string Build(IEnumerable<string> input)
+        {
+            return string.Format("_{0}", base.Build(input));
+        }
+    }
+}
