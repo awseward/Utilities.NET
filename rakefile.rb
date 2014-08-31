48639135603f6c507993cb72bf2884be2c99652c
Andrew Seward <awswrd@gmail.com>
2014-08-30 22:29:22 -0700

    Add rake cleaning stuff

---
+++ rakefile.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+
+def create_config
+  sh "cp config.example.yml config.yml"
+end
+
+create_config if !File.file?('config.yml')
+
+config = YAML.load_file('config.yml')
+
+namespace :clean do
+  do_not_clean = config['do_not_clean']
+  exclude_string = do_not_clean.map{ |str| "-e \"#{str}\"" }.join(' ')
+  clean_base = 'git clean -xdf'
+  dry_flag = '-n'
+
+  task :clean do
+    sh "#{clean_base} #{exclude_string}"
+  end
+
+  task :dry do
+    sh "#{clean_base} #{dry_flag} #{exclude_string}"
+  end
+end
