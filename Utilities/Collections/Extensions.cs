a26f59d1f2430b8588d407d37530a9756cca0300
Andrew Seward <awswrd@gmail.com>
2014-08-30 17:45:53 -0700

    Add some collections stuff

---
+++ Utilities/Collections/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities.Collections
+{
+    public static class Extensions
+    {
+        public static bool IsEmpty<T>(this IEnumerable<T> collection)
+        {
+            return collection.Count() == 0;
+        }
+
+        public static IEnumerable<T> Tail<T>(this IEnumerable<T> collection)
+        {
+            if (collection.Count() <= 1) { return Enumerable.Empty<T>(); }
+
+            return collection.Skip(1);
+        }
+    }
+}
