a9b945cc795e6695527b348a8819f4b6c2ccaf52
Andrew Seward <awswrd@gmail.com>
2014-08-30 22:14:00 -0700

    Add String/Misc For (mostly useless) alpha stuff right now

---
+++ Utilities/String/Misc/Alphabet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities.String.Misc
+{
+    public static class Alphabet
+    {
+        public static IEnumerable<char> Lower
+        {
+            get
+            {
+                return AlphabetStartingWith('a');
+            }
+        }
+
+        public static IEnumerable<char> Upper
+        {
+            get
+            {
+                return AlphabetStartingWith('A');
+            }
+        }
+
+        private static IEnumerable<char> AlphabetStartingWith(char ch)
+        {
+            for (int i = (int) ch; i < ch + 26; i++)
+            {
+                yield return (char) i;
+            }
+        }
+    }
+}
---
+++ Utilities/String/Misc/Extensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities.String.Misc
+{
+    public static class Extensions
+    {
+        public static int Count(this string str, char target)
+        {
+            return str.Where(ch => ch == target).Count();
+        }
+
+        public static IDictionary<char, int> CharacterCounts(this string str, IEnumerable<char> charSet)
+        {
+            var dict = new Dictionary<char, int>();
+
+            foreach (var ch in charSet)
+            {
+                dict.Add(ch, str.Count(ch));
+            }
+
+            return dict;
+        }
+
+        public static IDictionary<char, int> LowerAlphaCounts(this string str)
+        {
+            return str.CharacterCounts(Alphabet.Lower);
+        }
+
+        public static IDictionary<char, int> UpperAlphaCounts(this string str)
+        {
+            return str.CharacterCounts(Alphabet.Upper);
+        }
+
+        public static IDictionary<char, int> AlphaCounts(this string str)
+        {
+            var upper = str.UpperAlphaCounts();
+            var lower = str.LowerAlphaCounts();
+
+            return upper.Concat(lower).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        }
+
+        public static IDictionary<char, int> CaseInsensitiveAlphaCounts(this string str)
+        {
+            return str.ToUpper().UpperAlphaCounts();
+        }
+    }
+}
