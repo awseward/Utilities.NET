feae3ba923cd3cfcc891477310b5624ec54be815
Andrew Seward <awswrd@gmail.com>
2014-08-30 17:21:44 -0700

    Move all the stuff Nothing should say StringUtils anymore

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-StringUtils
-===========
+Utilities
+=========
--- Utilities/Casing/Cases/LeadingUnderscoreCamel.cs
+++ Utilities/Casing/Cases/LeadingUnderscoreCamel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace StringUtils.Casing.Cases
+namespace Utilities.Casing.Cases
 {
     public class LeadingUnderscoreCamel : Camel
     {
