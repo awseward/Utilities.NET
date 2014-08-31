f070759c5f111c9c44dcaded3bc321e8e24483fd
Andrew Seward <awswrd@gmail.com>
2014-08-30 19:08:35 -0700

    Add some http stuff

--- Utilities/Collections/Extensions.cs
+++ Utilities/Collections/Extensions.cs
@@ -25,5 +25,24 @@ namespace Utilities.Collections
 
             dict.Add(key, value.ToString());
         }
+
+        /// <remarks>Adapted from a stackoverlow answer.</remarks>
+        /// <see cref="http://stackoverflow.com/a/3804455"/>
+        public static bool IsEquivalentTo<TKey, TValue>(this IDictionary<TKey, TValue> thisD, IDictionary<TKey, TValue> thatD)
+        {
+            if (thisD == thatD) return true;
+            if (thisD == null || thatD == null) return false;
+            if (thisD.Count != thatD.Count) return false;
+
+            var comparer = EqualityComparer<TValue>.Default;
+
+            foreach (KeyValuePair<TKey, TValue> kvp in thisD)
+            {
+                TValue thatValue;
+                if (!thatD.TryGetValue(kvp.Key, out thatValue)) return false;
+                if (!comparer.Equals(kvp.Value, thatValue)) return false;
+            }
+            return true;
+        }
     }
 }
