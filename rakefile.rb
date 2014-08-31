a2361de95f623d89602eae7dc11fdb44ac2ae7bb
Andrew Seward <awswrd@gmail.com>
2014-08-30 22:39:19 -0700

    Might as well add some descriptions

--- rakefile.rb
+++ rakefile.rb
@@ -8,6 +8,7 @@ create_config if !File.file?('config.yml')
 
 config = YAML.load_file('config.yml')
 
+desc "Removes files ignored by git, minus ones matching do_not_clean from config.yml"
 task :clean => 'clean:clean'
 
 namespace :clean do
@@ -16,10 +17,12 @@ namespace :clean do
   clean_base = 'git clean -xdf'
   dry_flag = '-n'
 
+  desc "Removes files ignored by git, minus ones matching do_not_clean from config.yml"
   task :clean do
     sh "#{clean_base} #{exclude_string}"
   end
 
+  desc "Does a dry run of clean task"
   task :dry do
     sh "#{clean_base} #{dry_flag} #{exclude_string}"
   end
