d5e3005b63a315831201aa8dd3835184fafe9fe2
Andrew Seward <awswrd@gmail.com>
2014-09-01 17:04:18 -0700

    Add some interface stuff for StringCase

---
+++ Utilities/IRebuildable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities
+{
+    interface IRebuildable<TIn, TOut> : ISplittable<TOut, TIn> { }
+}
--- Utilities/String/Casing/Cases/StringCase.cs
+++ Utilities/String/Casing/Cases/StringCase.cs
@@ -5,10 +5,16 @@ using System.Text;
 
 namespace Utilities.String.Casing
 {
-    public abstract class StringCase
+    public abstract class StringCase : IRebuildable<IEnumerable<string>, string>
     {
         public abstract IEnumerable<string> Split(string input);
 
         public abstract string Build(IEnumerable<string> input);
+
+        public virtual IEnumerable<string> Deconstruct(string input)
+        {
+            return Split(input);
+        }
+
     }
 }
